fcef26ed754c75697ce5dbe036bcd5c9418fe648
dennisjoseph2025-dotcom <dennisjoseph2025@gmail.com>
2025-12-05 19:57:12 +0530

    update

--- src/store/slices/quizSlice.jsx
+++ src/store/slices/quizSlice.jsx
@@ -2,11 +2,11 @@ import { createSlice } from '@reduxjs/toolkit';
 import { firebaseService } from '../../services/firebaseService';
 
 // Import house images
-// import gryffindorIcon from '../../assets/gryffindor.png';
-// import slytherinIcon from '../../assets/slytherin.png';
-// import hufflepuffIcon from '../../assets/hufflepuff.png';
-// import ravenclawIcon from '../../assets/ravenclaw.png';
-// import mediaIcon from '../../assets/media.png';
+import gryffindorIcon from '../../assets/gryffindor.png';
+import slytherinIcon from '../../assets/slytherin.png';
+import hufflepuffIcon from '../../assets/hufflepuff.png';
+import ravenclawIcon from '../../assets/ravenclaw.png';
+import mediaIcon from '../../assets/media.png';
 
 const houses = [
   { 
@@ -14,7 +14,7 @@ const houses = [
     name: 'Gryffindor', 
     color: 'gryffindor', 
     bgColor: 'bg-gryffindor', 
-    icon: '../../assets/gryffindor.png',  // Use public path
+    icon: gryffindorIcon,
     adminPoints: 0,
     totalPoints: 0,
     isScoring: false
@@ -24,7 +24,7 @@ const houses = [
     name: 'Slytherin', 
     color: 'slytherin', 
     bgColor: 'bg-slytherin', 
-    icon: '../../assets/slytherin.png',  // Use public path
+    icon: slytherinIcon,
     adminPoints: 0,
     totalPoints: 0,
     isScoring: false
@@ -34,7 +34,7 @@ const houses = [
     name: 'Hufflepuff', 
     color: 'hufflepuff', 
     bgColor: 'bg-hufflepuff', 
-    icon: '../../assets/hufflepuff.png',  // Use public path
+    icon: hufflepuffIcon,
     adminPoints: 0,
     totalPoints: 0,
     isScoring: false
@@ -44,7 +44,7 @@ const houses = [
     name: 'Ravenclaw', 
     color: 'ravenclaw', 
     bgColor: 'bg-ravenclaw', 
-    icon: '../../assets/ravenclaw.png',  // Use public path
+    icon: ravenclawIcon,
     adminPoints: 0,
     totalPoints: 0,
     isScoring: false
@@ -54,7 +54,7 @@ const houses = [
     name: 'Media Team', 
     color: 'media', 
     bgColor: 'bg-media', 
-    icon: '../../assets/media.png',  // Use public path
+    icon: mediaIcon,
     adminPoints: 0,
     totalPoints: 0,
     isScoring: false
